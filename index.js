58816c82ba00e64af28350e801b4c3ea3bf8c63e
Satoshi Nagano <snagano@salesforce.com>
2020-07-14 15:48:40 +0900

    Update index.js
    update

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+require("appdynamics").profile({
+  controllerHostName: 'apm-demo-dynamics-5940.saas.appdynamics.com',
+  controllerPort: 443,
+  controllerSslEnabled: true,      // Set to false if controllerPort is not SSL enabled
+  accountName: 'apm-demo-dynamics-5940',
+  accountAccessKey: '8lxkegqvcs4f', //required
+  applicationName: 'apm-demo-dynamics', // Enter a name for your application
+  tierName: 'SampleNodejs-webtier',   // Enter custom defined tier name
+  nodeName: 'SampleHeroku-dyno',   // Enter custom defined node name
+ });
+
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
@@ -8,14 +19,3 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-  require("appdynamics").profile({
-  controllerHostName: 'apm-demo-dynamics-5940.saas.appdynamics.com',
-  controllerPort: 443,
-  controllerSslEnabled: true,      // Set to false if controllerPort is not SSL enabled
-  accountName: 'apm-demo-dynamics-5940',
-  accountAccessKey: '8lxkegqvcs4f', //required
-  applicationName: 'apm-demo-dynamics', // Enter a name for your application
-  tierName: 'SampleNodejs-webtier',   // Enter custom defined tier name
-  nodeName: 'SampleHeroku-dyno',   // Enter custom defined node name
- });
